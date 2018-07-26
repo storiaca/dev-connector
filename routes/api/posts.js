f1b8a234d91744db5d38f8d347d9e2a9e717f6d6
Aleksandar Ristic <storiaca@gmail.com>
2018-07-26 11:28:00 +0200

    Post create route fix

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,19 +6,29 @@ const passport = require('passport');
 // Load Post Model
 const Post =  require('../../models/Post');
 
+// Validation
+const validatePostInput = require('../../validation/post');
+
 // @route  GET api/posts/test
 // @desc   Tests posts route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
 
-// @route  GET api/posts/test
-// @desc   Tests posts route
+// @route  Posts api/posts
+// @desc   Create post
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+  // Check Validation
+  if(!isValid) {
+    // If any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+
   const newPost = new Post({
     text: req.body.text,
     name: req.body.name,
-    avatar: req.body.name,
+    avatar: req.body.avatar,
     user: req.user.id
   });
 
