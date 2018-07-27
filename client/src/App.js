a2062375ef6ecb519ab16a39af60f093152cae15
Aleksandar Ristic <storiaca@gmail.com>
2018-07-27 15:06:17 +0200

    Create Layout, Footer and Navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+
+// Components
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My react app</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
