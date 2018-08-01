f01bf140ae05858cff5aa3480d98aed51273722c
Aleksandar Ristic <storiaca@gmail.com>
2018-08-01 15:22:40 +0200

    Profile reducer, profile action, get current profile

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,10 @@ import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
+import { clearCurrentProfile } from './actions/profileActions';
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -29,8 +31,8 @@ if(localStorage.jwtToken) {
   if(decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
-    
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
   }
@@ -46,6 +48,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
