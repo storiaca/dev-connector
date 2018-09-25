f3eb4e35d308c41321d270506568331ca74ea8b5
Aleksandar Ristic <storiaca@gmail.com>
2018-09-25 14:29:44 +0200

    Create add experience action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -20,6 +20,19 @@ export const getCurrentProfile = () => dispatch => {
     );
 }
 
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post('./api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure, this can NOT be undone!!")) {
