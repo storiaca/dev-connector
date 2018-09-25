320bedc10e39adaefefd3df10c2abcd394a48880
Aleksandar Ristic <storiaca@gmail.com>
2018-09-25 13:06:27 +0200

    Add Experience form inputs

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,12 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/common/PrivateRoute';
 import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
-import EditProfile from './components/edit-profile/EditProfile';
+
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -59,6 +61,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <EditProfile exact path="/edit-profile" component={EditProfile} />
+              </Switch>
+              <Switch>
+                <AddExperience exact path="/add-experience" component={AddExperience} />
               </Switch>
             </div>
             <Footer />
