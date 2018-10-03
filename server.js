1c5bb13b37a04435a26db9602512af3fc887cd69
Aleksandar Ristic <storiaca@gmail.com>
2018-10-03 16:18:56 +0200

    Prepared for deploy fix

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api/posts', posts);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(epxress.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
